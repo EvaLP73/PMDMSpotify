deed386de138156b81594b4dccee530325378dc9
Eva <105455442+EvaLP73@users.noreply.github.com>
2025-10-02 19:17:21 +0200

    empezamos spotify

--- app/src/main/java/com/example/spotify/pages/SelectorPage.kt
+++ app/src/main/java/com/example/spotify/pages/SelectorPage.kt
@@ -2,13 +2,14 @@ package com.example.spotify.pages
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.spotify.R
 import com.example.spotify.myComponents.ImageTextHorizontal
 import com.example.spotify.myComponents.ImageTextSquare
 
 @Composable
 fun SelectorPage(){
-    ImageTextHorizontal( "prueba", imageIdR = 1,) { }
-    ImageTextSquare("texto1","texto2", imageIdR = 1) { }
+    ImageTextHorizontal( "prueba", R.drawable.imagen1) { }
+    ImageTextSquare("texto1", "texto2",R.drawable.imagen1) { }
 }
 
 @Preview
